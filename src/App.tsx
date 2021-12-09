395de155ce32fce06afa2143d1f67d1553ec6b36
Pavel Golub <pavel.golub@gmail.com>
2021-12-09 16:02:49 +0300

    Added tests. Fixedlayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import Box from '@mui/material/Box';
-import React, {useRef, useState} from 'react';
+import { useRef, useState } from 'react';
 // import logo from './logo.svg';
 // import './App.css';
-import {PortSelector} from "./PortSelector";
-import {Container, LinearProgress, Stack, TextField, ThemeProvider} from "@mui/material";
-import {createTheme} from '@mui/material/styles';
-import {logging} from "./log/LogManager";
-import {Progress} from "./dc/progress";
-import {SuuntoVyperDevice} from "./dc/suuntoVyperDevice";
-import {DataGrid} from '@mui/x-data-grid';
-import {DivesGrid} from "./DivesGrid";
-import {Dive} from "./dc/diveProfile";
+import { PortSelector } from "./PortSelector";
+import { Container, Grid, TextField, ThemeProvider } from "@mui/material";
+import { createTheme } from '@mui/material/styles';
+import { logging } from "./log/LogManager";
+import { Progress } from "./dc/progress";
+import { SuuntoVyperDevice } from "./dc/suuntoVyperDevice";
+import { DivesGrid } from "./DivesGrid";
+import { Dive } from "./dc/diveProfile";
+import LinearProgress from '@mui/material/LinearProgress';
 
 const theme = createTheme({
     palette: {
@@ -35,12 +35,13 @@ logging
 
 function App() {
     // setup logging
-    let [log, setLog] = useState("");
+    let [log] = useState("");
     const [progress, setProgress] = useState(0);
-    const [dives, setDives] = useState<Array<Dive>>(new Array<Dive>(0));
+    const [dives, setDives] = useState<Array<Dive>>(new Array<Dive>());
+    let dvs = new Array<Dive>();
     logging.onLogEntry(logEntry => {
         log += `${logEntry.time.getHours()}:${logEntry.time.getMinutes()}:${logEntry.time.getSeconds()}.${logEntry.time.getMilliseconds()} ${logEntry.level.toUpperCase()} [${logEntry.module}] ${logEntry.message}\n`;
-// logArea.current
+        // logArea.current
         //TODO: check the log set properly
         // setLog(() => log);
     });
@@ -52,19 +53,20 @@ function App() {
 
     //setup progress indicator
     let updateProgress = function (event: Progress) {
-        setProgress(event.total == 0 ? 0 : (event.current / event.total) * 100);
+        setProgress(event.total === 0 ? 0 : (event.current / event.total) * 100);
     }
 
+    function addDive(dive: Dive) {
+        dvs.push(dive);
+        setDives(dvs.map((x) => x));
+    }
     //setup
     let getDivesCallback = async function (port: SerialPort) {
         let device = new SuuntoVyperDevice(port, updateProgress);
         try {
             logger.info((await device.open()).toString());
-            //TODO: use emitter or a callback?
-            let dives1 = await device.getDives();
-            logger.info(dives1.toString());
-            setDives(dives1);
-        } catch (e: unknown) {
+            await device.getDives(addDive);
+        } catch (e) {
             logger.error((e as Error).toString());
         } finally {
             await device.close()
@@ -74,22 +76,27 @@ function App() {
     // @ts-ignore
     return (
         <ThemeProvider theme={theme}>
-            <Box sx={{display: 'flex', flexDirection: 'column', gap: '30px'}}>
-                <Box sx={{height: 200}}/>
-                <PortSelector getDivesCallback={getDivesCallback}/>
-                <LinearProgress variant="determinate" value={progress}/>
-                <DivesGrid dives={dives}/>
-                <TextField
-                    id="log-area"
-                    ref={logArea}
-                    label="Log"
-                    multiline
-                    sx={{minHeight: 300}}
-                    maxRows={10}
-                    value={log}
-                />
-            </Box>
-        </ThemeProvider>
+            <Grid container>
+                <Grid item xs={3}></Grid>
+                <Grid item xs={6}>
+                    <Box sx={{ height: 100 }} />
+                    <PortSelector getDivesCallback={getDivesCallback} />
+                    ///https://mui.com/components/progress/
+                    <LinearProgress variant="determinate" value={progress} sx={{ padding: 1, marginBottom: 2, marginTop: 2 }} />
+                    <DivesGrid dives={dives} />
+                    <TextField
+                        id="log-area"
+                        ref={logArea}
+                        label="Log"
+                        multiline
+                        sx={{ minHeight: 300, width: 1, marginTop: 2 }}
+                        maxRows={10}
+                        value={log}
+                    />
+                    <Grid item xs={3}></Grid>
+                </Grid>
+            </Grid>
+        </ThemeProvider >
     );
 }
 
